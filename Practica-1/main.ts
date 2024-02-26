60095059575b27a61e18149d6b9a1166096efea3
JaimeGC <jgcarbajo1@gmail.com>
2024-02-26 19:38:16 +0100

    Practica-1

--- Practica-1/main.ts
+++ Practica-1/main.ts
@@ -4,9 +4,9 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 
-import "$std/dotenv/load.ts";
+import "https://deno.land/std@0.211.0/dotenv/load.ts";
 
-import { start } from "$fresh/server.ts";
+import { start } from "https://deno.land/x/fresh@1.6.5/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
 
