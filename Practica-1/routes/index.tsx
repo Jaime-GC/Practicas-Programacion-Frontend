4134067e781e8dd8c7741eed577169bdb70058ee
JaimeGC <jgcarbajo1@gmail.com>
2024-03-07 11:48:51 +0100

    Practica-2

---
+++ Practica-1/routes/index.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+    return (
+      <div class="container">
+        <h1 >Practica 1: elige el tipo de leccion</h1>
+
+        <div id="botones">
+        <a class="button" href="/leccion_aleatoria">Leccion Aleatoria</a>
+        <a class="button" href="/leccion_elegida">Leccion Elegida</a>
+        </div>
+
+
+        <div class="anuncio-der"></div>
+        <div class="anuncio-izq"></div>
+        
+      </div>
+    );
+  }
+  
