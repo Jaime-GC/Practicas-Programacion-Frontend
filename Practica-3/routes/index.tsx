3bc2f07b237f17f60e3d2fe977be1c4ac45065ca
Arturo Gónzalez Carbajo <agcarbajo1@gmail.com>
2024-04-12 14:21:04 +0200

    Revert "Practica 4"
    This reverts commit 1ee42f6c1e20c094aeacc1ff4be053e99fe3759c.

--- Practica-3/routes/index.tsx
+++ Practica-3/routes/index.tsx
@@ -1,45 +1,25 @@
-import axios from "npm:axios";
-import Lover from "../components/Lover.tsx";
-import { LoverType } from "../types.ts";
-import { comment } from "../types.ts";
-import FilteredProfilesPage from "../islands/FilteredProfilesPage.tsx";
+import { useSignal } from "@preact/signals";
+import Counter from "../islands/Counter.tsx";
 
-
-
-export default async function Home() {
-
-    const response = await axios.get<LoverType[]>(
-      "https://lovers.deno.dev/",
-    );
-
-    const lovers:Lover[] = response.data;
-    return (
-      <>
-      <div>
-
-        <FilteredProfilesPage />
+export default function Home() {
+  const count = useSignal(3);
+  return (
+    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+        <img
+          class="my-6"
+          src="/logo.svg"
+          width="128"
+          height="128"
+          alt="the Fresh logo: a sliced lemon dripping with juice"
+        />
+        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+        <p class="my-4">
+          Try updating this message in the
+          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+        </p>
+        <Counter count={count} />
       </div>
-
-
-
-
-
-        <div class="flex-column">
-          <h1 class="mainTitle">Lovers</h1>
-            <div class="flex-row flex-around">
-     
-                {lovers && lovers.map((lover) => (
-                  <Lover
-                    name={lover.name}
-                    photo={lover.photo}
-                  />
-                ))}
-
-            </div>
-        </div>
-
-
-      </>
-    );
-
+    </div>
+  );
 }
